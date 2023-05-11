92a74bf6cb2516e62f6be61beaa878bbb0799c62
kostiscpp <81684506+kostiscpp@users.noreply.github.com>
2023-05-11 23:13:58 +0300

    Update db1_initial_01.sql

--- backend/db1_initial_01.sql
+++ backend/db1_initial_01.sql
@@ -1,3 +1,10 @@
+
+DROP DATABASE IF EXISTS db1initial;
+
+CREATE DATABASE db1initial;
+
+USE db1initial;
+
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
@@ -78,15 +85,57 @@ DELIMITER ;
 --
 
 CREATE TABLE `author` (
-  `author_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `author_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (author_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `book`
+-- Table structure for table `school`
 --
+
+CREATE TABLE `school` (
+  `school_id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_name` varchar(255) NOT NULL,
+  `address` varchar(60) NOT NULL,
+  `city` varchar(30) NULL,
+  `phone_number` bigint(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `principal_name` varchar(60) NOT NULL,
+  `school_admin_name` varchar(60) NOT NULL,
+  PRIMARY KEY (`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publisher`
+--
+
+CREATE TABLE `publisher` (
+  `publisher_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`publisher_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+-- 
 
 CREATE TABLE `book` (
   `ISBN` varchar(20) NOT NULL,
@@ -98,21 +147,27 @@ CREATE TABLE `book` (
   `image` varchar(255) NOT NULL DEFAULT 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeE4BZwuwU8ODTBIrNIoSs4xxVuOU2XSzQTQ&usqp=CAU',
   `language` varchar(40) NOT NULL DEFAULT 'English',
   `key-words` varchar(70) NOT NULL DEFAULT 'Key words of the book',
-  `year_published` year(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `year_published` year(4) NOT NULL,
+  PRIMARY KEY (ISBN),
+  CONSTRAINT `FK_PUBLISHER_ID` FOREIGN KEY (publisher_id) REFERENCES `publisher` (publisher_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `book_copy`
 --
 
+-- --------------------------------------------------------
+
 CREATE TABLE `book_copy` (
-  `copy_id` int(11) NOT NULL,
+  `copy_id` int(11) NOT NULL AUTO_INCREMENT,
   `book_id` varchar(20) NOT NULL,
   `dewey_code` varchar(8) NOT NULL DEFAULT '800.0xxx' COMMENT 'https://en.wikipedia.org/wiki/List_of_Dewey_Decimal_classes ',
-  `school_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `school_id` int(11) NOT NULL,
+  PRIMARY KEY (`copy_id`),
+  CONSTRAINT `FK_ISBN` FOREIGN KEY (`book_id`) REFERENCES `book` (`ISBN`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_SCHOOL_ID` FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -120,120 +175,29 @@ CREATE TABLE `book_copy` (
 -- Table structure for table `book_to_author`
 --
 
+-- --------------------------------------------------------
+
 CREATE TABLE `book_to_author` (
   `author_id` int(11) NOT NULL,
-  `ISBN` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
+  `ISBN` varchar(20) NOT NULL,
+  CONSTRAINT `FK_AUTHOR_ID` FOREIGN KEY (author_id) REFERENCES `author` (author_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_AUTH_ISBN` FOREIGN KEY (ISBN) REFERENCES `book` (ISBN) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `book_to_category`
 --
 
+-- --------------------------------------------------------
+
 CREATE TABLE `book_to_category` (
   `category_id` int(11) NOT NULL,
-  `ISBN` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ISBN` varchar(20) NOT NULL,
+  CONSTRAINT `FK_CATEGORY_ID` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_CAT_ISBN` FOREIGN KEY (ISBN) REFERENCES `book` (ISBN) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
-
-CREATE TABLE `category` (
-  `category_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `checkout`
---
-
-CREATE TABLE `checkout` (
-  `checkout_id` int(11) NOT NULL,
-  `start_time` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `end_time` datetime NOT NULL,
-  `is_returned` tinyint(1) NOT NULL,
-  `book_copy_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hold`
---
-
-CREATE TABLE `hold` (
-  `hold_id` int(11) NOT NULL,
-  `start_time` datetime NOT NULL,
-  `end_time` datetime NOT NULL,
-  `book_copy_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `hold_status` enum('pending','active') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `publisher`
---
-
-CREATE TABLE `publisher` (
-  `publisher_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pwd`
---
-
-CREATE TABLE `pwd` (
-  `pwd_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `creation_date` datetime NOT NULL,
-  `expiration_date` datetime NOT NULL,
-  `failed_attempts` int(11) NOT NULL,
-  `password_policy` enum('Weak','Medium','Strong') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `review`
---
-
-CREATE TABLE `review` (
-  `review_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `review_text` longtext NOT NULL,
-  `rating` int(5) NOT NULL,
-  `book_id` varchar(50) NOT NULL,
-  `review_status` enum('rejected','pending','accepted') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `school`
---
-
-CREATE TABLE `school` (
-  `school_id` int(11) NOT NULL,
-  `school_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `address` varchar(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `city` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `phone_number` bigint(10) NOT NULL,
-  `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `principal_name` varchar(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `school_admin_name` varchar(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -242,10 +206,10 @@ CREATE TABLE `school` (
 --
 
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL UNIQUE,
   `pwd` varchar(50) NOT NULL,
   `birth_date` date NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -253,223 +217,107 @@ CREATE TABLE `user` (
   `status` enum('active','inactive','deleted') NOT NULL,
   `role` enum('admin','school-admin','teacher','student') NOT NULL,
   `profile` varchar(255) NOT NULL DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/300px-User-avatar.svg.png',
-  `barcode` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `barcode` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `FK_SCH_ID` FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Table structure for table `checkout`
 --
 
---
--- Indexes for table `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`author_id`);
+CREATE TABLE `checkout` (
+  `checkout_id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_time` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `end_time` datetime NOT NULL,
+  `is_returned` tinyint(1) NOT NULL,
+  `book_copy_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`checkout_id`),
+  CONSTRAINT `FK_C_COPY_ID` FOREIGN KEY (book_copy_id) REFERENCES `book_copy` (copy_id),
+  CONSTRAINT `FK_USER_ID` FOREIGN KEY (user_id) REFERENCES `user` (user_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
 
 --
--- Indexes for table `book`
---
-ALTER TABLE `book`
-  ADD PRIMARY KEY (`ISBN`),
-  ADD KEY `FK_PUBLISHER_ID` (`publisher_id`);
-
---
--- Indexes for table `book_copy`
---
-ALTER TABLE `book_copy`
-  ADD PRIMARY KEY (`copy_id`),
-  ADD KEY `FK_SCHOOL_ID` (`school_id`),
-  ADD KEY `FK_ISBN` (`book_id`);
-
---
--- Indexes for table `book_to_author`
---
-ALTER TABLE `book_to_author`
-  ADD KEY `FK_ISBN` (`ISBN`) USING BTREE,
-  ADD KEY `FK_AUTHOR_ID` (`author_id`);
-
---
--- Indexes for table `book_to_category`
---
-ALTER TABLE `book_to_category`
-  ADD KEY `FK_CATEGORY_ID` (`category_id`);
-
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`category_id`);
-
---
--- Indexes for table `checkout`
---
-ALTER TABLE `checkout`
-  ADD PRIMARY KEY (`checkout_id`),
-  ADD KEY `FK_CHECKOUT_BID` (`book_copy_id`),
-  ADD KEY `FK_CHECKOUT_USR_ID` (`user_id`);
-
---
--- Indexes for table `hold`
---
-ALTER TABLE `hold`
-  ADD PRIMARY KEY (`hold_id`),
-  ADD KEY `FK_HOLD_BID` (`book_copy_id`),
-  ADD KEY `FK_UID` (`user_id`);
-
---
--- Indexes for table `publisher`
---
-ALTER TABLE `publisher`
-  ADD PRIMARY KEY (`publisher_id`);
-
---
--- Indexes for table `pwd`
---
-ALTER TABLE `pwd`
-  ADD PRIMARY KEY (`pwd_id`),
-  ADD KEY `FK_PWD_USER_ID` (`user_id`);
-
---
--- Indexes for table `review`
---
-ALTER TABLE `review`
-  ADD PRIMARY KEY (`review_id`),
-  ADD KEY `FK_REVIEW_USER` (`user_id`),
-  ADD KEY `FK_REVIEW_BOOK` (`book_id`);
-
---
--- Indexes for table `school`
---
-ALTER TABLE `school`
-  ADD PRIMARY KEY (`school_id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`),
-  ADD KEY `FK_SCH_ID` (`school_id`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Table structure for table `hold`
 --
 
---
--- AUTO_INCREMENT for table `author`
---
-ALTER TABLE `author`
-  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT;
+CREATE TABLE `hold` (
+  `hold_id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `book_copy_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `hold_status` enum('pending','active') NOT NULL,
+  PRIMARY KEY (`hold_id`),
+  CONSTRAINT `FK_H_COPY_ID` FOREIGN KEY (`book_copy_id`) REFERENCES `book_copy` (`copy_id`),
+  CONSTRAINT `FK_UID` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+-- --------------------------------------------------------
 
 --
--- AUTO_INCREMENT for table `book_copy`
---
-ALTER TABLE `book_copy`
-  MODIFY `copy_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `checkout`
---
-ALTER TABLE `checkout`
-  MODIFY `checkout_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `hold`
---
-ALTER TABLE `hold`
-  MODIFY `hold_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `publisher`
---
-ALTER TABLE `publisher`
-  MODIFY `publisher_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pwd`
---
-ALTER TABLE `pwd`
-  MODIFY `pwd_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `review`
---
-ALTER TABLE `review`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `school`
---
-ALTER TABLE `school`
-  MODIFY `school_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
+-- Table structure for table `pwd`
 --
 
---
--- Constraints for table `book`
---
-ALTER TABLE `book`
-  ADD CONSTRAINT `FK_PUBLISHER_ID` FOREIGN KEY (`publisher_id`) REFERENCES `publisher` (`publisher_id`);
+-- --------------------------------------------------------
+
+CREATE TABLE `pwd` (
+  `pwd_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `expiration_date` datetime NOT NULL,
+  `failed_attempts` int(11) NOT NULL,
+  `password_policy` enum('Weak','Medium','Strong') NOT NULL,
+  PRIMARY KEY (`pwd_id`),
+  CONSTRAINT `FK_P_UID` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+-- --------------------------------------------------------
 
 --
--- Constraints for table `book_copy`
+-- Table structure for table `review`
 --
-ALTER TABLE `book_copy`
-  ADD CONSTRAINT `FK_ISBN` FOREIGN KEY (`book_id`) REFERENCES `book` (`ISBN`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `FK_SCHOOL_ID` FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- --------------------------------------------------------
+
+CREATE TABLE `review` (
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `review_text` longtext NOT NULL,
+  `rating` int(5) NOT NULL,
+  `book_id` varchar(50) NOT NULL,
+  `review_status` enum('rejected','pending','accepted') NOT NULL,
+  PRIMARY KEY (`review_id`),
+  CONSTRAINT `FK_R_ISBN` FOREIGN KEY (`book_id`) REFERENCES `book` (`ISBN`),
+  CONSTRAINT `FK_R_USER_ID` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+-- --------------------------------------------------------
 
 --
--- Constraints for table `book_to_author`
+-- Table structure for table `keyword`
 --
-ALTER TABLE `book_to_author`
-  ADD CONSTRAINT `FK_AUTHOR_ID` FOREIGN KEY (`author_id`) REFERENCES `author` (`author_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `FK_ISBNN` FOREIGN KEY (`ISBN`) REFERENCES `book` (`ISBN`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Constraints for table `book_to_category`
---
-ALTER TABLE `book_to_category`
-  ADD CONSTRAINT `FK_CATEGORY_ID` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- --------------------------------------------------------
 
---
--- Constraints for table `checkout`
---
-ALTER TABLE `checkout`
-  ADD CONSTRAINT `FK_CCOPY_ID` FOREIGN KEY (`book_copy_id`) REFERENCES `book_copy` (`copy_id`),
-  ADD CONSTRAINT `FK_USER_ID` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+CREATE TABLE `keyword` (
+  `keyword_id` int(11) NOT NULL AUTO_INCREMENT,
+  `keyword` varchar(255) NOT NULL,
+  PRIMARY KEY (keyword_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
---
--- Constraints for table `hold`
---
-ALTER TABLE `hold`
-  ADD CONSTRAINT `FK_HCOPY_ID` FOREIGN KEY (`book_copy_id`) REFERENCES `book_copy` (`copy_id`),
-  ADD CONSTRAINT `FK_UID` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+CREATE TABLE `book_to_keyword` (
+  `keyword_id` int(11) NOT NULL,
+  `ISBN` varchar(20) NOT NULL,
+  CONSTRAINT `FK_KW_ISBN` FOREIGN KEY (`ISBN`) REFERENCES `book` (`ISBN`),
+  CONSTRAINT `FK_KW_ID` FOREIGN KEY (`keyword_id`) REFERENCES `keyword` (`keyword_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
---
--- Constraints for table `review`
---
-ALTER TABLE `review`
-  ADD CONSTRAINT `FK_RISBN` FOREIGN KEY (`book_id`) REFERENCES `book` (`ISBN`),
-  ADD CONSTRAINT `FK_RUSER_ID` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
-
---
--- Constraints for table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `FK_SCH_ID` FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
