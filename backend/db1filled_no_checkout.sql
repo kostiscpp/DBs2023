6cfa0879a470f17c39cd78acdc3dd1b3cc63b83c
kostiscpp <81684506+kostiscpp@users.noreply.github.com>
2023-05-13 15:57:44 +0300

    Update db1filled_no_checkout.sql

--- backend/db1filled_no_checkout.sql
+++ backend/db1filled_no_checkout.sql
@@ -78,35 +78,7 @@ END$$
 DELIMITER ;
 
 
-DELIMITER //
 
-CREATE TRIGGER tr_more_than_two
-BEFORE INSERT ON checkout
-FOR EACH ROW
-BEGIN
-  DECLARE borrow_count INT;
-  SELECT COUNT(*) INTO borrow_count FROM checkout WHERE user_id = NEW.user_id;
-  IF borrow_count >= 2 THEN
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot borrow more than 2 books.';
-  END IF;
-END //
-
-CREATE TRIGGER tr_check_overdue
-BEFORE INSERT ON checkout
-FOR EACH ROW
-BEGIN
-
-  IF (
-    SELECT COUNT(*) FROM checkout
-    WHERE user_id = NEW.user_id
-      AND return_time IS NULL
-      AND DATEDIFF(CURDATE(), checkout_time) > 7
-  ) > 0 THEN
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot borrow books, you have an overdue book checkout.';
-  END IF;
-END //
-
-DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -349,7 +321,35 @@ CREATE TABLE `book_to_keyword` (
   CONSTRAINT `FK_KW_ID` FOREIGN KEY (`keyword_id`) REFERENCES `keyword` (`keyword_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
+DELIMITER //
 
+CREATE TRIGGER tr_more_than_two
+BEFORE INSERT ON checkout
+FOR EACH ROW
+BEGIN
+  DECLARE borrow_count INT;
+  SELECT COUNT(*) INTO borrow_count FROM checkout WHERE user_id = NEW.user_id;
+  IF borrow_count >= 2 THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot borrow more than 2 books.';
+  END IF;
+END //
+
+CREATE TRIGGER tr_check_overdue
+BEFORE INSERT ON checkout
+FOR EACH ROW
+BEGIN
+
+  IF (
+    SELECT COUNT(*) FROM checkout
+    WHERE user_id = NEW.user_id
+      AND return_time IS NULL
+      AND DATEDIFF(CURDATE(), checkout_time) > 7
+  ) > 0 THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot borrow books, you have an overdue book checkout.';
+  END IF;
+END //
+
+DELIMITER ;
 
 INSERT INTO school (school_name, address, city, phone_number, email, principal_name, operator_name)
 VALUES
