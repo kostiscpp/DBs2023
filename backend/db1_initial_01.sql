e2149d5cd8be96651bb6b97ff08d6163a772f8a8
kostiscpp <81684506+kostiscpp@users.noreply.github.com>
2023-05-18 16:23:49 +0300

    Update db1_initial_01.sql

--- backend/db1_initial_01.sql
+++ backend/db1_initial_01.sql
@@ -323,7 +323,7 @@ CREATE TABLE `book_to_keyword` (
 
 DELIMITER //
 
-CREATE TRIGGER tr_more_than_two
+CREATE TRIGGER tr_more_than_two_ch
 BEFORE INSERT ON checkout
 FOR EACH ROW
 BEGIN
@@ -334,7 +334,7 @@ BEGIN
   END IF;
 END //
 
-CREATE TRIGGER tr_check_overdue
+CREATE TRIGGER tr_check_overdue_ch
 BEFORE INSERT ON checkout
 FOR EACH ROW
 BEGIN
@@ -348,6 +348,68 @@ BEGIN
     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot borrow books, you have an overdue book checkout.';
   END IF;
 END //
+
+CREATE TRIGGER tr_same_book_ch
+BEFORE INSERT ON checkout
+FOR EACH ROW
+BEGIN
+
+  IF (
+    SELECT COUNT(*) FROM checkout c
+    WHERE c.user_id = NEW.user_id
+      AND c.return_time IS NULL
+      AND ((SELECT book_id FROM book_copy WHERE NEW.book_copy_id = copy_id) = (SELECT book_id FROM book_copy WHERE c.book_copy_id = copy_id)) 
+  ) > 0 THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot borrow 2 copies of the same book.';
+  END IF;
+END //
+
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE TRIGGER tr_more_than_two_h
+BEFORE INSERT ON hold
+FOR EACH ROW
+BEGIN
+  DECLARE hold_count INT;
+  SELECT COUNT(*) INTO hold_count FROM hold WHERE user_id = NEW.user_id;
+  IF hold_count >= 2 THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot reserve more than 2 books.';
+  END IF;
+END //
+
+CREATE TRIGGER tr_check_overdue_h
+BEFORE INSERT ON hold
+FOR EACH ROW
+BEGIN
+
+  IF (
+    SELECT COUNT(*) FROM checkout
+    WHERE user_id = NEW.user_id
+      AND return_time IS NULL
+      AND DATEDIFF(CURDATE(), checkout_time) > 7
+  ) > 0 THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot borrow books, you have an overdue book checkout.';
+  END IF;
+END //
+
+CREATE TRIGGER tr_same_book_h
+BEFORE INSERT ON hold
+FOR EACH ROW
+BEGIN
+
+  IF (
+    SELECT COUNT(*) FROM hold h
+    WHERE h.user_id = NEW.user_id
+      AND h.return_time IS NULL
+      AND ((SELECT book_id FROM book_copy WHERE NEW.book_copy_id = copy_id) = (SELECT book_id FROM book_copy WHERE h.book_copy_id = copy_id)) 
+  ) > 0 THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot hold 2 copies of the same book.';
+  END IF;
+END //
+
 
 DELIMITER ;
 
