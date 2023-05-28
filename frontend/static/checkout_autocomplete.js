91119e9369a82b5792dac3c983e864e97fdf81b5
kostiscpp <81684506+kostiscpp@users.noreply.github.com>
2023-05-29 00:21:24 +0300

    Update checkout_autocomplete.js

--- frontend/static/checkout_autocomplete.js
+++ frontend/static/checkout_autocomplete.js
@@ -1,6 +1,8 @@
+console.log("Loaded scripts.js");
 $(document).ready(function() {
   // Book Autocomplete Button Click Event
   $('#bookAutocompleteBtn').click(function() {
+    console.log("Button comple");
     var isbn = $('#isbnInput').val();
     // Make an asynchronous request to your server to fetch book info based on the ISBN
     // Update the book info fields with the returned data
@@ -8,11 +10,12 @@ $(document).ready(function() {
       url: '/checkout/autocomplete/book',
       method: 'GET',
       data: { isbn: isbn },
-      success: function(book) {
-        $('#bookTitle').val(book.title);
-        $('#bookAuthor').val(book.author);
-        $('#bookPages').val(book.pages);
-        $('#bookYear').val(book.year);
+      success: function(response) {
+        var books = JSON.parse(response);
+        $('#bookTitle').val(books[0].title);
+        $('#bookAuthor').val(books[0].author);
+        $('#bookPages').val(books[0].pages);
+        $('#bookYear').val(books[0].year);
       },
       error: function(error) {
         console.log(error);
@@ -29,12 +32,13 @@ $(document).ready(function() {
       url: '/checkout/autocomplete/user',
       method: 'GET',
       data: { barcode: barcode },
-      success: function(user) {
-        $('#userFirstName').val(user.first_name);
-        $('#userSurname').val(user.surname);
-        $('#userRole').val(user.role);
-        $('#userSchool').val(user.school_name);
-        $('#userEmail').val(user.email);
+      success: function(response) {
+        var users = JSON.parse(response);
+        $('#userFirstName').val(users[0].first_name);
+        $('#userSurname').val(users[0].surname);
+        $('#userRole').val(users[0].role);
+        $('#userSchool').val(users[0].school_name);
+        $('#userEmail').val(users[0].email);
       },
       error: function(error) {
         console.log(error);
