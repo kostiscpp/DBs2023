29c6bf6e27f59eddad0080a605011fb1cb8b076f
MariosRozos <132021871+MariosRozos@users.noreply.github.com>
2023-06-01 09:55:50 +0300

    Add files via upload

---
+++ frontend/static/checkout_autocomplete.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  // Book Autocomplete Button Click Event
+  $('#bookAutocompleteBtn').click(function() {
+    var isbn = $('#isbnInput').val();
+    // Make an asynchronous request to your server to fetch book info based on the ISBN
+    // Update the book info fields with the returned data
+    $.ajax({
+      url: '/checkout/autocomplete/book',
+      method: 'GET',
+      data: { isbn: isbn },
+      success: function(response) {
+        var books = JSON.parse(response);
+        $('#bookTitle').val(books[0].title);
+        $('#bookAuthor').val(books[0].author);
+        $('#bookPages').val(books[0].pages);
+        $('#bookYear').val(books[0].year);
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
+  });
+
+  // User Autocomplete Button Click Event
+  $('#userAutocompleteBtn').click(function() {
+    var barcode = $('#barcodeInput').val();
+    // Make an asynchronous request to your server to fetch user info based on the barcode
+    // Update the user info fields with the returned data
+    $.ajax({
+      url: '/checkout/autocomplete/user',
+      method: 'GET',
+      data: { barcode: barcode },
+      success: function(response) {
+        var users = JSON.parse(response);
+        $('#userFirstName').val(users[0].first_name);
+        $('#userSurname').val(users[0].surname);
+        $('#userRole').val(users[0].role);
+        $('#userSchool').val(users[0].school_name);
+        $('#userEmail').val(users[0].email);
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
+  });
+});
